fcba9cb7e0caf8fb61b0c134666c4867dc84f48d
Consistometru <ady_cosmin07@yahoo.com>
2021-10-30 15:55:03 +0000

    init

---
+++ try-exception.py
@@ -0,0 +1,31 @@
+print("welcome")
+from operator import pow, truediv, mul, add, sub, floordiv, mod
+w = 0
+operators = {
+  '+': add,
+  '-': sub,
+  '*': mul,
+  '/': truediv,
+  '^': pow,
+  '//': floordiv,
+  '%': mod
+}
+
+def calculate(s):
+    if s.isdigit():
+        return float(s)
+    for c in operators.keys():
+        left, operator, right = s.partition(c)
+        if operator in operators:
+            return operators[operator](calculate(left), calculate(right))
+if w:
+     file = open("history.txt", "w")
+input1 = input("continue? yes / no: ")
+while input1 == "yes":
+    calc = input("Type calculation:\n")
+    print("Answer: " + str(calculate(calc)))
+    if w:
+         file.write(calc + " Answer: " + str(calculate(calc)))
+    input1 = input("continue? yes / no: ")
+if w:
+     file.close()
